71accd938502f119b258bd8d7b51c791013214fe
Andelo <andelo.batinic@lernende.bbw.ch>
2022-01-06 16:11:27 +0100

    Initial commit

---
+++ src/test/java/ch/bbw/ab/savecalculator/AppTest.java
@@ -0,0 +1,23 @@
+package ch.bbw.ab.savecalculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ *
+ * @author Andelo Batinic
+ * @version 6. Januar 2022
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
